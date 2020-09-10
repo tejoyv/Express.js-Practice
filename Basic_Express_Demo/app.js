f955dc706e7f9352031d83ac668487d45a7ac08f
Tejoy Vachhrajani <tejoyv@gmail.com>
2020-09-10 10:38:12 +0530

    Create html file for calculator

--- Basic_Express_Demo/app.js
+++ Basic_Express_Demo/app.js
@@ -5,18 +5,6 @@ app.get("/",function(req,res){
 	res.send("<h1>Hello</h1>");
 })
 
-app.get("/contact",function(req,res){
-	res.send("contact details");
-})
-
-app.get("/about",function(req,res){
-	res.send("<h1>Tejoy Vachhrajani</h1>");
-})
-
-app.get("/location",function(req,res){
-	res.send("Vadodara");
-})
-
 app.listen("3000",function(){
 	console.log("Express Server running on port 3000");
 })
