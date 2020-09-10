f2821e7804427663f7096964716c699410e6f852
Tejoy Vachhrajani <tejoyv@gmail.com>
2020-09-10 17:18:37 +0530

    JS date formating

--- Todo_list_v1/app.js
+++ Todo_list_v1/app.js
@@ -8,34 +8,14 @@ app.set('view engine', 'ejs');
 app.get("/",function(req,res){
 
 	var today = new Date();
-	var day = today.getDay();
 
-	switch(day){
-		case 0:
-			day="Sunday";
-			break;
-		case 1:
-			day="Monday";
-			break;
-		case 2:
-			day="Tuesday";
-			break;
-		case 3:
-			day="Wednesday";
-			break;
-		case 4:
-			day="Thursday";
-			break;
-		case 5:
-			day="Friday";
-			break;
-		case 6:
-			day="Saturday";
-			break;
-		default:
-			day="Error";
-			console.log(day);
+	var options = {
+		weekday:"long",
+		day:"numeric",
+		month:"long"
 	}
+
+	var day = today.toLocaleDateString("en-US",options);
 
 	res.render("list",{todayIsDay:day});
 })
