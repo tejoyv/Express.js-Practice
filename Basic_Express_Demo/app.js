eaecae85c777d717b55bb0d199627595ff0bf2e8
Tejoy Vachhrajani <tejoyv@gmail.com>
2020-09-10 10:32:10 +0530

    Task of own url

--- Basic_Express_Demo/app.js
+++ Basic_Express_Demo/app.js
@@ -9,6 +9,10 @@ app.get("/contact",function(req,res){
 	res.send("contact details");
 })
 
+app.get("/about",function(req,res){
+	res.send("<h1>Tejoy Vachhrajani</h1>");
+})
+
 app.listen("3000",function(){
 	console.log("Express Server running on port 3000");
 })
