9a1f725050f82627c38a4cbdf0f79ea708d9049c
Tejoy Vachhrajani <tejoyv@gmail.com>
2020-09-10 10:30:08 +0530

    handle the request query

--- Basic_Express_Demo/app.js
+++ Basic_Express_Demo/app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/",function(req,res){
-	res.send("Hello");
+	res.send("<h1>Hello</h1>");
+})
+
+app.get("/contact",function(req,res){
+	res.send("contact details");
 })
 
 app.listen("3000",function(){
