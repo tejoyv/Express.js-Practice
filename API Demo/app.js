79969b33339d58a05993ab61bf5e5e980491e368
Tejoy Vachhrajani <tejoyv@gmail.com>
2020-09-10 09:33:54 +0530

    make request

--- API Demo/app.js
+++ API Demo/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const https = require("https");
+require('dotenv').config()
+
 const app = express();
 
 app.get("/",function(req,res){
-	res.send("Server is running");
+
+	var url = "https://api.openweathermap.org/data/2.5/weather?q=Vadodara&appid="+ process.env.WEATHER_API_KEY +"&units=metric";
+
+	https.get(url,function(response){
+		console.log(response);
+	})
 })
 
 app.listen("3000",function(req,res){
