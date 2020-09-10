831685fa2a4e3e6d48795e75e07769f2631a211d
Tejoy Vachhrajani <tejoyv@gmail.com>
2020-09-10 12:40:53 +0530

    created signup page

--- Newsletter_Signup/app.js
+++ Newsletter_Signup/app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
-	res.send("Server response");
+	res.sendFile(__dirname+"/signup.html");
 })
+
 
 app.listen("3000",function(){
 	console.log("Express Server running on port 3000");
