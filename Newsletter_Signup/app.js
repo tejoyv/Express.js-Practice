65bcb9b464bfb694912b1e2033242921c86b3e11
Tejoy Vachhrajani <tejoyv@gmail.com>
2020-09-10 13:38:32 +0530

    Create data to be passed as json to mailchimp

--- Newsletter_Signup/app.js
+++ Newsletter_Signup/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+require('dotenv').config()
 
 const app = express();
 
@@ -11,14 +12,34 @@ app.get("/",function(req,res){
 })
 
 app.post("/",function(req,res){
+	//console.log("Post request received");
 	var fname = req.body.fname;
 	var lname = req.body.lname;
 	var email = req.body.email;
 
-	console.log(fname,lname,email);
+	const data = {
+		members : [{
+			email_address: email,
+			status: "subscribed",
+			merge_fields:{
+				// Audience->Settings->Audience fields and MERGE tags
+				FNAME:fname,    
+				LNAME:lname
+			}
+		}]
+	}
+
 })
 
 
 app.listen("3000",function(){
 	console.log("Express Server running on port 3000");
 })
+
+
+// API KEY
+// process.env.API_KEY
+
+// List ID (List you want to keep the subscribers into)
+// process.env.List_ID
+
