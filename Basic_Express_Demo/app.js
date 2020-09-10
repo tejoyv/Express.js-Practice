ccee9cad89d7b2d3c6074c7de1d3d2d07aad16e3
Tejoy Vachhrajani <tejoyv@gmail.com>
2020-09-10 10:27:41 +0530

    Print the request

--- Basic_Express_Demo/app.js
+++ Basic_Express_Demo/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/",function(req,res){
+	console.log(req);
+})
+
 app.listen("3000",function(){
 	console.log("Express Server running on port 3000");
 })
