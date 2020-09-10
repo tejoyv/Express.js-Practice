3ff3b824718c91519c0d875acec86bc30e84bde6
Tejoy Vachhrajani <tejoyv@gmail.com>
2020-09-10 11:01:10 +0530

    Task BMI calculator

--- Basic_Express_Demo/app.js
+++ Basic_Express_Demo/app.js
@@ -9,12 +9,24 @@ app.get("/",function(req,res){
 	//_dirname automatically find the project dir
 })
 
+app.get("/bmi",function(req,res){
+	res.sendFile(__dirname+"/bmi.html");
+})
+
 app.post("/",function(req,res){
 	var num1 = Number(req.body.num1);
 	var num2 = Number(req.body.num2);
 	var result = num1+num2;
 
 	res.send("The result of the calculation is : "+result);
+})
+
+app.post("/bmi",function(req,res){
+	var weight = req.body.weight;
+	var height = req.body.height;
+	var bmi = weight/(height*height);
+
+	res.send("Your BMI score is : "+bmi);
 })
 
 app.listen("3000",function(){
