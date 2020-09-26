7d51618ebf32d8066461665817cf4f5cdc9c78d3
Tejoy Vachhrajani <tejoyv@gmail.com>
2020-09-26 22:36:17 +0530

    Display data on Screen load it from DB

--- Todo_list_v2/app.js
+++ Todo_list_v2/app.js
@@ -36,20 +36,25 @@ const item3 = new Item({
 
 const defaultItems = [item1,item2,item3];
 
-Item.insertMany(defaultItems,function(err){
-  if(err){
-    console.log(err);
-  }else{
-    console.log("Successfully saved default items to DB");
-  }
-});
-
 app.get("/", function(req, res) {
 
+  Item.find({},function(err,foundItems){
 
-
-  res.render("list", {listTitle: "Today", newListItems: items});
-
+    //foundItems is an array check by console logging
+    //this is for default items
+    if(foundItems.length === 0){
+      Item.insertMany(defaultItems,function(err){
+      if(err){
+          console.log(err);
+        }else{
+          console.log("Successfully saved default items to DB");
+        }
+      });
+      res.redirect("/");
+    }else{
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  });
 });
 
 app.post("/", function(req, res){
