b5759336959df8ee4b8de2f75ec9634f46a3eb34
Tejoy Vachhrajani <tejoyv@gmail.com>
2020-09-26 23:49:20 +0530

    Add the items to dynamic list

--- Todo_list_v2/app.js
+++ Todo_list_v2/app.js
@@ -97,13 +97,23 @@ app.get("/:customListName",function(req,res){
 app.post("/", function(req, res){
 
   const itemName = req.body.newItem;
+  const listName = req.body.list;
 
   const item = new Item({
     name: itemName
   });
 
-  item.save();
-  res.redirect("/");
+  if(listName === "Today"){
+    item.save();
+    res.redirect("/");
+  }else{
+    //find the custom list and add the new item to that custom list
+    List.findOne({name:listName},function(err,foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/"+listName);
+    })
+  }
 
 });
 
